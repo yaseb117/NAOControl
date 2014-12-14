e81607a4e63ba81d5e4baf18be5cf2eb2c3bb00d
Elyes <bselyes@gmail.com>
2014-12-14 22:52:16 +0100

    UPDATE

--- gen/com/edu/naocontrol/R.java
+++ gen/com/edu/naocontrol/R.java
@@ -28,7 +28,8 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int main_activity=0x7f030000;
+        public static final int splash_screen=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/edu/NAO/SplashScreen.java
+++ src/com/edu/NAO/SplashScreen.java
@@ -1,16 +1,30 @@
-package com.edu.naocontrol;
+package com.edu.NAO;
+
+import com.edu.naocontrol.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class SplashScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.splash_screen);
+		Handler handler=new Handler();
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashScreen.this,MainActivity.class);
+				startActivity(i);
+				finish();				
+			}
+		}, 2000);
 	}
 
 	@Override
